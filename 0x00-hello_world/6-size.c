23d3236bd1f7dbc2eeab712b79b7a1d4361fe21a
Yousra <y.haimeur7@gmail.com>
2023-03-20 12:34:41 -0700

    4-puts.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the size of various computer types 
+ * return 0 (Success)
+ */
+int main(void)
+{       
+        char a;
+        int b;
+        long int c;
+        long long int d;
+        float f;
+printf("Size of a char: %lu bytes(s)\n", (unsigned long), sizeofof(a));
+printf("Size of an art: %lu bytes(s)\n", (unsigned long), sizeofof(b));
+printf("Size of a long int: %lu bytes(s)\n", (unsigned long), sizeofo(c));
+printf("Size of a long long int: %lu bytes(s)\n", (unsigned long), sizeofof(d));
+printf("Size of a float: %lu bytes(s)\n", (unsigned long), sizeofof(f));
+return (0);
+}
