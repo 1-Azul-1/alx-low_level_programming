d789107347a03888604773f46ce17668b813a0d7
Yousra <y.haimeur7@gmail.com>
2023-03-22 11:54:47 -0700

    readme file created

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
- * main -Prints the size of various types based on
+ * main - Prints the size of various types based on
  * the computer it is compiled and run on..
  * return: Always 0.
  */
 int main(void)
 {
 
-printf("Size of a char: %zu byte(s)\n", (unsigned long), sizeof(char));
-printf("Size of an int: %zu byte(s)\n", (unsigned long), sizeof(int));
-printf("Size of a long int: %zu byte(s)\n", (unsigned long), sizeof(long int));
-printf("Size of a long long int: %zu byte(s)\n", (unsigned long),
-sizeof(long long int));
-printf("Size of a float: %zu byte(s)\n", (unsigned long), sizeof(float));
+printf("Size of a char: %d byte(s)\n", (unsigned long), sizeof(char));
+printf("Size of an int: %d byte(s)\n", (unsigned long), sizeof(int));
+printf("Size of a long int: %d byte(s)\n", (unsigned long), sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", (unsigned long), sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", (unsigned long), sizeof(float));
+
 return (0);
 }
--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,10 +8,9 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{       
-        int n:
-                
-        srand(time(0));
+{    
+int n:
+  	srand(time(0));
         n = rand() - RAHD_MAX / 2;
         if (n > 0)
         {       
