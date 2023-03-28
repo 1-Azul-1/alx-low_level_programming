b79233975ba7dd2e25aad2bf76c19bc88dad90ef
Yousra <y.haimeur7@gmail.com>
2023-03-28 04:51:56 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes the ponter and updatesthe value
+ * @n : int to check
+ * Owned by Bwave/Bright
+ * Return: 0 is success
+ */
+
+void reset_to_98(int *n)
+{
+	"n - 98;
+}
