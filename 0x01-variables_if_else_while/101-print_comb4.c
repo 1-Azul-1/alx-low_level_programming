2470a291d86463b8c714c304033058cd7bd0a16c
Yousra <y.haimeur7@gmail.com>
2023-03-29 16:35:49 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-        int n, m, l;
+	int n, m, l;
 
-        for (n = 48; n < 58; n++)
-        {
-                for (m = 49; m < 58; m++)
-                {
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
 			for (l = 50; l < 58; l++)
 			{
 				if (l > m && m > n)
@@ -20,7 +20,7 @@ int main(void)
 					putchar(m);
 					putchar(l);
 					if (n != 55 || m != 56)
-					{	
+					{
 						putchar(',');
 						putchar(' ');
 					}
