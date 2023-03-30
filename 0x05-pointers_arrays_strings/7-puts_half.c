8e46e6f0795d28653e3e62d4ad475f3d173e38cc
Yousra <y.haimeur7@gmail.com>
2023-03-30 08:12:37 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,19 +1,20 @@
 #include "main.h"
 /**
  * _strlen - return the length of a string
- * Owned by Bwave
- * @s: char to check
- * Description: this will return the length of a string
- * Return: 0 is success
+ * @s: string
+ * Return: length
  */
-
 int _strlen(char *s)
 {
-        int a = 0;
+	int longi = 0;
 
-        for (; *s++;)
-                a++;
-        return (a);
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
 }
 
 
