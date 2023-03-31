513034bfccc66b2c4a53cc5db7888fe5e0753376
Yousra <y.haimeur7@gmail.com>
2023-03-31 15:21:46 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,21 +6,22 @@
  */
 void print_number(int n)
 {
-	unsigned int nl;
+	unsigned int n;
 
 	if (n < 0)
 	{
 		nl = -n;
+		{
 		_putchar('-');
-	} else
+	}else
 	{
 		nl = n;
 	}
 
 	if (nl / 10)
 	{
-		print_number(nl / 10};
+		print_number(nl / 10);
 	}
-
+	
 	_putchar((nl % 10) + '0');
 }
