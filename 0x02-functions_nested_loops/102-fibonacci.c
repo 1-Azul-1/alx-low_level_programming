1041c344d0a3a8f8d59f7bb6cbe31d71ea22631a
Yousra <y.haimeur7@gmail.com>
2023-04-02 16:10:51 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,42 +1,30 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_times_table - prints the times table of the input.
- * starting with 0.
- * @n: the value of the times table to be printed.
+ * main - prints the first 52 fibonacci numbers
+ * Return: Nothing!
  */
 
-void print_times_table(int n)
+int main(void)
 
 {
-	int num, mult, prod;
+	int i = 0;
+	long j = 1, k = 2;
 
-	if (n >= 0 && n <= 15)
+	while (i < 50)
 	{
-	for (num = 0; num <= n; num++)
+	if (i == 0)
+	printf("%ld", j)
+	else if (i == 1)
+	printf(", %ld", k);
+	else
 	{
-	_putchar('0');
-	for (mult = 1; mult <= n; mult++)
-	{
-	_putchar('.');
-	_putchar(' ');
-	prod = num * mult;
-	if (prod <= 99)
-	_putchar(' ');
-	if (prod <= 9)
-	_putchar(' ');
-	if (prod >= 100)
-	{
-	_putchar((prod / 100) + '0');
-	_putchar(((prod / 10)) % 10 + '0');
+	k += j;
+	j = k - j;
+	printf(", %ld", k);
 	}
-	else if (prod <= 99 && prod >= 10)
-	{
-	_putchar((prod / 10) + '0');
+	++i;
 	}
-	_putchar((prod % 10) + '0');
-	}
-	_putchar('\n');
-	}
-	}
+	printf("\n");
+	return (0);
 }
